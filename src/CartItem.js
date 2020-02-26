4cf009001a1f63d44793844aba6e6452332ae856
codingninjasstudio <user@example.com>
2020-02-26 19:17:54 +0530

    modify state

--- src/CartItem.js
+++ src/CartItem.js
@@ -12,7 +12,19 @@ class CartItem extends React.Component {
     // this.increaseQuantity = this.increaseQuantity.bind(this);
   }
   increaseQuantity = () => {
-    console.log('this', this.state);
+    // this.state.qty += 1;
+    // console.log('this', this.state);
+    // setState form 1
+    // this.setState({
+    //   qty: this.state.qty + 1
+    // });
+
+    // setState form 2 - if prevState required use this
+    this.setState((prevState) => {
+      return {
+        qty: prevState.qty + 1
+      }
+    });
   }
   render () {
     const { price, title, qty } = this.state;
