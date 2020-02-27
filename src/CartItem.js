321444535d50be5c6531f30343ee9bf4a3f644d9
codingninjasstudio <user@example.com>
2020-02-27 15:04:54 +0530

    setState in depth - I

--- src/CartItem.js
+++ src/CartItem.js
@@ -24,9 +24,26 @@ class CartItem extends React.Component {
       return {
         qty: prevState.qty + 1
       }
+    }, () => {
+      console.log('this.state', this.state);
+    });
+  }
+
+  decreaseQuantity = () => {
+    const { qty } = this.state;
+
+    if (qty === 0) {
+      return;
+    }
+    // setState form 2 - if prevState required use this
+    this.setState((prevState) => {
+      return {
+        qty: prevState.qty - 1
+      }
     });
   }
   render () {
+    console.log('render');
     const { price, title, qty } = this.state;
     return (
       <div className="cart-item">
@@ -49,6 +66,7 @@ class CartItem extends React.Component {
               alt="decrease"
               className="action-icons"
               src="https://image.flaticon.com/icons/svg/1665/1665612.svg"
+              onClick={this.decreaseQuantity}
             />
             <img
               alt="delete"
