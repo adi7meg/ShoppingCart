8e4dfc34df3c85e7ee6e0a61884daf4635179c5b
codingninjasstudio <user@example.com>
2020-02-27 18:18:42 +0530

    delete product from cart

--- src/Cart.js
+++ src/Cart.js
@@ -43,6 +43,30 @@ class Cart extends React.Component {
       products
     })
   }
+  handleDecreaseQuantity = (product) => {
+    console.log('Heyy please inc the qty of ', product);
+    const { products } = this.state;
+    const index = products.indexOf(product);
+
+    if (products[index].qty === 0) {
+      return;
+    }
+
+    products[index].qty -= 1;
+
+    this.setState({
+      products
+    })
+  }
+  handleDeleteProduct = (id) => {
+    const { products } = this.state;
+
+    const items = products.filter((item) => item.id !== id); // [{}]
+
+    this.setState({
+      products: items
+    })
+  }
   render () {
     const { products } = this.state;
     return (
@@ -53,6 +77,8 @@ class Cart extends React.Component {
               product={product}
               key={product.id}
               onIncreaseQuantity={this.handleIncreaseQuantity}
+              onDecreaseQuantity={this.handleDecreaseQuantity}
+              onDeleteProduct={this.handleDeleteProduct}
             />
           )
         })}
