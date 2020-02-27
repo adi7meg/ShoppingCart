b6f64791a3de42b73d64fcef89cd8dc127fe96b2
codingninjasstudio <user@example.com>
2020-02-27 16:51:30 +0530

    rendering list

--- src/Cart.js
+++ src/Cart.js
@@ -3,11 +3,12 @@ import CartItem from './CartItem';
 
 class Cart extends React.Component {
   render () {
+    const arr = [1, 2, 3, 4, 5];
     return (
       <div className="cart">
-        <CartItem />
-        <CartItem />
-        <CartItem />
+        { arr.map((item) => {
+          return item + 5
+        }) }
       </div>
     );
   }
